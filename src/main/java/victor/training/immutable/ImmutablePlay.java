109ee2724bfcd5b80e73fdd6ee058e6703095c20
Victor Rentea <victorrentea@gmail.com>
2021-02-12 22:48:38 +0200

    constructor validation

--- src/main/java/victor/training/immutable/ImmutablePlay.java
+++ src/main/java/victor/training/immutable/ImmutablePlay.java
@@ -2,6 +2,7 @@ package victor.training.immutable;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.toList;
@@ -19,27 +20,31 @@ public class ImmutablePlay {
 
 class A {
    private final int x;
-   private final List<Integer> numbers;
-   private final B other;
+   private final List<Integer> list;
+   private final B b; // line 1
 
-   public A(int x, List<Integer> numbers, B other) {
+   public A(int x, List<Integer> list, B b) {
       this.x = x;
-      this.numbers = numbers;
-      this.other = other;
+      this.list = Objects.requireNonNull(list);
+      this.b = Objects.requireNonNull(b);
+   }
+   public List<Integer> getList() {
+      return Collections.unmodifiableList(list); // line 3
    }
    public int getX() {
       return x;
    }
-   public List<Integer> getNumbers() {
-      return numbers;
+
+   public A withX(int newX) { // line 4
+      return new A(newX, list, b);
    }
-   public B getOther() {
-      return other;
+   public B getB() {
+      return b;
    }
 
    @Override
    public String toString() {
-      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, numbers, other);
+      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, list, b);
    }
 }
 
