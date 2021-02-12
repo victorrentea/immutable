4bb4cc00fe110c82345175a93a90c4966fbc2cfd
Victor Rentea <victorrentea@gmail.com>
2021-02-13 00:04:11 +0200

    add Lombok

--- src/main/java/victor/training/immutable/A.java
+++ src/main/java/victor/training/immutable/A.java
@@ -1,38 +1,23 @@
 package victor.training.immutable;
 
+import lombok.NonNull;
+import lombok.Value;
+import lombok.With;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
+@Value
 public class A {
-   private final int x;
-   private final List<Integer> list;
-   private final B b; // line 1
-
-   public A(int x, List<Integer> list, B b) {
-      this.x = x;
-      this.list = Objects.requireNonNull(list);
-      this.b = Objects.requireNonNull(b);
-   }
+   @With
+   int x;
+   @NonNull
+   List<Integer> list;
+   @NonNull
+   B b;
 
    public List<Integer> getList() {
-      return Collections.unmodifiableList(list); // line 3
-   }
-
-   public int getX() {
-      return x;
-   }
-
-   public A withX(int newX) { // line 4
-      return new A(newX, list, b);
-   }
-
-   public B getB() {
-      return b;
-   }
-
-   @Override
-   public String toString() {
-      return String.format("Immutable{x=%d, numbers=%s, other=%s}", x, list, b);
+      return Collections.unmodifiableList(list);
    }
 }
